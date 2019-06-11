96086ccafe26987aef904fb9246495cc641e9662
user <user@lab2095m.tumo.lab>
2019-06-11 19:01:00 +0400

    final

---
+++ lesson_2/modules/grass.js
@@ -0,0 +1,18 @@
+var living_creature = require("./living_creature");
+var random = require("./random");
+
+module.exports = class Grass extends living_creature {
+
+    mult() {
+        var empty = random(this.chooseCell(0));
+        this.multiply++;
+        if (empty && this.multiply > 1) {
+            var x = empty[0]
+            var y = empty[1]
+            matrix[y][x] = 1
+            var newGr = new Grass(x, y, 1)
+            grassArr.push(newGr)
+            this.multiply = 0
+        }
+    }
+}
---
+++ lesson_2/server.js
@@ -0,0 +1,115 @@
+
+//! Requiring modules  --  START
+var Grass = require("./modules/grass.js");
+var Xotaker = require("./modules/xotaker.js");
+let random = require('./modules/random');
+//! Requiring modules  --  END
+
+
+//! Setting global arrays  --  START
+grassArr = [];
+xotakerArr = [];
+matrix = [];
+grassHashiv = 0;
+//! Setting global arrays  -- END
+
+
+
+
+//! Creating MATRIX -- START
+function matrixGenerator(matrixSize, grass, xotaker, grassEaterEater, waterArr, fireArr) {
+    for (let i = 0; i < matrixSize; i++) {
+        matrix[i] = [];
+        for (let o = 0; o < matrixSize; o++) {
+            matrix[i][o] = 0;
+        }
+    }
+    for (let i = 0; i < grass; i++) {
+        let customX = Math.floor(random(matrixSize)); // 0-9
+        let customY = Math.floor(random(matrixSize)); // 4
+        matrix[customY][customX] = 1;
+    }
+    for (let i = 0; i < xotaker; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 2;
+    }
+    for (let i = 0; i < grassEaterEater; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 3;
+    }
+    for (let i = 0; i < waterArr; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 4;
+    }
+    for (let i = 0; i < fireArr; i++) {
+        let customX = Math.floor(random(matrixSize));
+        let customY = Math.floor(random(matrixSize));
+        matrix[customY][customX] = 5;
+    }
+}
+matrixGenerator(20, 1, 1);
+//! Creating MATRIX -- END
+
+
+
+//! SERVER STUFF  --  START
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+app.use(express.static("."));
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+server.listen(3000);
+//! SERVER STUFF END  --  END
+
+
+
+function creatingObjects() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 2) {
+                var grassEater = new Xotaker(x, y);
+                xotakerArr.push(grassEater);
+            } else if (matrix[y][x] == 1) {
+                var grass = new Grass(x, y);
+                grassArr.push(grass);
+                grassHashiv++;
+            }
+        }
+    }
+}
+creatingObjects();
+
+function game() {
+    if (grassArr[0] !== undefined) {
+        for (var i in grassArr) {
+            grassArr[i].mult();
+        }
+    }
+    if (xotakerArr[0] !== undefined) {
+        for (var i in xotakerArr) {
+            xotakerArr[i].eat();
+            xotakerArr[i].move();
+            xotakerArr[i].mult();
+            xotakerArr[i].die();
+        }
+    }
+
+    //! Object to send
+    let sendData = {
+        matrix: matrix,
+        grassCounter: grassHashiv
+    }
+
+    //! Send data over the socket to clients who listens "data"
+    io.sockets.emit("data", sendData);
+}
+
+
+
+setInterval(game, 100);
